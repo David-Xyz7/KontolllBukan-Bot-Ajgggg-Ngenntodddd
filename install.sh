4bbe2b2f5787c80e93bfa14dcdfdd9c5aa806719
MR.D7 <81473463+dreamteam14@users.noreply.github.com>
2021-06-19 10:24:20 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,16 @@
 
 apt-get update
 apt-get upgrade
-apt-get install -y nodejs libwebp ffmpeg wget tesseract
+npm i nodejs libwebp ffmpeg wget tesseract
+npm i lolis.life
+npm i awesome-phonenumber
+npm i cfonts
+npm i spinnies
+npm i brainly-scraper
+npm i remove.bg
+npm i image-to-base64
+npm i base64-img
+npm i imgbb-uploader
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
 
